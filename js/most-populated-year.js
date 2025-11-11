b43f2f4691a6d84388b3a3e0096e3ca95beb2a4d
qeryz <czkmaz@gmail.com>
2025-11-11 11:11:46 -0800

    nlogn solution

--- js/most-populated-year.js
+++ js/most-populated-year.js
@@ -7,19 +7,47 @@ let input = [
 ];
 
 function mostPopulatedYear(data) {
-  let yearPopulation = {};
+  //   let yearPopulation = {};
+  //   for (const person of data) {
+  //     let birthyear = person[0];
+  //     let years = person[1];
+  //     for (let i = birthyear; i < birthyear + years; i++) {
+  //       yearPopulation[i] = (yearPopulation[i] || 0) + 1;
+  //     }
+  //   }
+  //   const sortedPopulation = Object.entries(yearPopulation).sort(
+  //     (a, b) => b[1] - a[1]
+  //   );
+
+  //   return sortedPopulation[0][0];
+
+  let yearsPopCounter = {};
+  let maxPopulationYear;
+  let maxPopulation = 0;
+
   for (const person of data) {
-    let birthyear = person[0];
-    let years = person[1];
-    for (let i = birthyear; i < birthyear + years; i++) {
-      yearPopulation[i] = (yearPopulation[i] || 0) + 1;
+    let birthYear = person[0];
+    let yearsAlive = person[1];
+    let deathYear = birthYear + yearsAlive;
+
+    // Add birth year to unique years list. Then, increment population counter for year
+    yearsPopCounter[birthYear] = (yearsPopCounter[birthYear] || 0) + 1;
+
+    // Add death year to unique years list. Then, decrement population counter for year
+    yearsPopCounter[deathYear] = (yearsPopCounter[deathYear] || 0) - 1;
+  }
+
+  let uniqueYears = Object.keys(yearsPopCounter).sort((a, b) => a - b);
+
+  let population = 0;
+  for (const year of uniqueYears) {
+    population += yearsPopCounter[year];
+    if (population > maxPopulation) {
+      maxPopulation = population;
+      maxPopulationYear = year;
     }
   }
-  const sortedPopulation = Object.entries(yearPopulation).sort(
-    (a, b) => b[1] - a[1]
-  );
-
-  return sortedPopulation[0][0];
+  return maxPopulationYear;
 }
 
 console.log(util.inspect(mostPopulatedYear(input), { depth: null }));
